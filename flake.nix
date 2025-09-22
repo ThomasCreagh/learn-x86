45905bbfc314a0e9c382c6a1a78e1b454f950ef8
Thomas Creagh <github@thomascreagh.mailer.me>
2025-09-22 13:30:40 +0100

    first test using uasm

--- flake.nix
+++ flake.nix
@@ -8,15 +8,19 @@
   outputs = { self, nixpkgs }:
   let
     system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
+        "uasm"
+      ];
+    };
   in
   {
     devShells.${system}.default = pkgs.mkShell {
       packages = [
         pkgs.uasm
       ];
-      env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      ];
+      env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ ];
     };
   };
 }
