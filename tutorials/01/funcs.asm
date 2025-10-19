6990f49104d8ca208ac40eac56da22cf478fe2f4
Thomas Creagh <github@thomascreagh.mailer.me>
2025-10-19 12:26:23 +0100

    extra part finished

--- tutorials/01/funcs.asm
+++ tutorials/01/funcs.asm
@@ -16,8 +16,17 @@ add_g:
 min:
 	push	ebp
 	mov	ebp, esp
-	mov	eax, [ebp+8]
-	add	eax, [g]
+	mov	eax, [ebp+8]	; get a
+	mov	edx, [ebp+12]	; get b
+	cmp	eax, edx	; is a < b
+	jl	.min_fst
+	mov	eax, edx	; replace a with the value in b
+.min_fst:
+	mov	edx, [ebp+16]	; get c
+	cmp	eax, edx	; is min(a, b) < c
+	jl	.min_snd
+	mov	eax, edx	; replace a with the value in b
+.min_snd:
 	leave
 	ret
 
