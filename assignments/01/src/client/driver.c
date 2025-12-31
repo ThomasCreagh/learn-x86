8ce57dd6b1cc7ce0ab99ea15bab976655ed49c3f
Thomas Creagh <github@thomascreagh.mailer.me>
2025-12-31 16:29:48 +0000

    breh

--- assignments/01/src/client/driver.c
+++ assignments/01/src/client/driver.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
 
-extern int start();
+extern int start(int argc, char **argv);
 
-int main() {
-  const char *fifo_server = "../../server.pipe";
-  const char *fifo_client = "../../anthony.pipe";
-
-  fd_server = open(fifo_server, O_RDONLY);
-  fd_client = open(fifo_client, O_WRONLY);
-
-  printf("server pipe fd is: %d\n", fd_server);
-  printf("calling server _start");
-  start(fd_server, fd_client);
-  printf("finished server execution");
-
-  return 0;
+int main(int argc, char **argv) {
+    printf("calling client _start\n");
+    start(argc, argv);
+    printf("finished client execution\n");
+    return 0;
 }
